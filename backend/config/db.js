7dda76a3810712804cc2e50936d0b083d0fb948e
balaganapathiA <115018255+balaganapathiA@users.noreply.github.com>
2024-12-18 15:26:31 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () =>{
-    await mongoose.connect('mongodb+srv://bala0428:Bala%40123@data.an06x.mongodb.net/reactjs-food-delivery-app?retryWrites=true&w=majority&appName=data').then(()=>{
+    await mongoose.connect('mongodb+srv://bala0428:Bala%40123@data.an06x.mongodb.net/github?retryWrites=true&w=majority&appName=data').then(()=>{
        console.log('DB connected') ;
     })
 }

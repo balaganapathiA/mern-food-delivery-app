76508e0f7bf1137b43387d2e4020d9e8309ea991
balaganapathiA <115018255+balaganapathiA@users.noreply.github.com>
2024-12-18 13:17:28 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe =  new Stripe(process.env.STRIPE_SECRET_KEY)
 
 // Placing user order for frontend
 const placeOrder = async (req, res) => {
-    const frontend_url = 'http://localhost:5173';
+    const frontend_url = 'https://mern-food-delivery-app-frontend-bjzq.onrender.com';
     try {
         const { userId, items, amount, address, paymentMethod } = req.body;
 
